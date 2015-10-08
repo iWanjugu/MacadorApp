e739e3af6ed07b97ce4a78f538c327dc0efb28d2
Wanjugu Irene <ireneemaina@gmail.com>
2015-10-08 21:29:55 +0300

    Forms: Incorporated into URLs, Views and Templates; Updating bookmarks from the form is a SUCCESS!

--- marcador/views.py
+++ marcador/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404, redirect, render
 
+from .forms import BookmarkForm
 from .models import Bookmark
 
 # Create your views here.
@@ -19,3 +22,38 @@ def bookmark_user(request, username):
         bookmarks = Bookmark.public.filter(owner__username=username)
     context = {'bookmarks': bookmarks, 'owner': user}
     return render(request, 'marcador/bookmark_user.html', context)
+
+# create new views for the newcly created form 'BookmarkForm'
+# View 1
+@login_required
+def bookmark_create(request):
+    if request.method == 'POST':
+        form = BookmarkForm(data=request.POST)
+        if form.is_valid():
+            bookmark = form.save(commit=False)
+            bookmark.owner = request.user
+            bookmark.save()
+            form.save_m2m()
+            return redirect('marcador_bookmark_user',
+                username=request.user.username)
+    else:
+        form = BookmarkForm()
+    context = {'form': form, 'create': True}
+    return render(request, 'marcador/form.html', context)
+
+# View 2
+@login_required
+def bookmark_edit(request, pk): #primary key
+    bookmark = get_object_or_404(Bookmark, pk=pk)
+    if bookmark.owner != request.user and not request.user.is_superuser:
+        raise PermissionDenied
+    if request.method == 'POST':
+        form = BookmarkForm(instance=bookmark, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('marcador_bookmark_user',
+                username=request.user.username)
+    else:
+        form = BookmarkForm(instance=bookmark)
+    context = {'form': form, 'create': False}
+    return render(request, 'marcador/form.html', context)
